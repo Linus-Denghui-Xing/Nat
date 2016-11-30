24c381395e830752e8089f87e405caae0bf2d5b2
Linus-Denghui-Xing <linus.dhx@gmail.com>
2016-11-30 11:29:44 -0800

    Update Cell_Para_Transfer.py

--- Cell_Para_Transfer.py
+++ Cell_Para_Transfer.py
@@ -4,6 +4,5 @@ A = [[1, 0],[-0.5,(3.0**0.5)*0.5]]  ## Old Vector in (U,V)^T
 
 B = [[2,1],[1,2]]  ## New Vector in (U',V')^T
 
-E = np.matmul(B,np.linalg.inv(A))
+E = np.matmul(B,np.linalg.inv(A)) ## The target transfer matrix in MS as the parameter of U, V
 print E,'\n'
-
