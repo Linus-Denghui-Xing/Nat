e952207048471d979059a66f04fb264326c0f4ce
Linus-Denghui-Xing <linus.dhx@gmail.com>
2016-11-30 11:28:39 -0800

    Add files via upload

---
+++ Cell_Para_Transfer.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+A = [[1, 0],[-0.5,(3.0**0.5)*0.5]]  ## Old Vector in (U,V)^T
+
+B = [[2,1],[1,2]]  ## New Vector in (U',V')^T
+
+E = np.matmul(B,np.linalg.inv(A))
+print E,'\n'
+
